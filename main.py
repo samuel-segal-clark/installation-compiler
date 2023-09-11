142aaccf945d800ad33dd72eab4cd05e3d177aeb
Samuel Segal <samuel.segal@clarkfoundationsllc.com>
2023-09-11 15:11:06 -0400

    Implemented python installing

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import configparser
 import os
+import urllib.request
+
+def install_python(python_path: str) -> None:
+    urlreq = urllib.request.urlopen(python_path)
+    with open(r'files\install\install-python.exe', 'w') as file:
+        file.buffer.write(urlreq.read())
+#TODO Check to see that python is not installed
 
 config = configparser.ConfigParser()
-config.read(u'files\install\config.ini')
+config.read(r'files\install\config.ini')
 
 python_path = config['DEFAULT']['pythonInstallExeUrl']
-
+install_python(python_path)
